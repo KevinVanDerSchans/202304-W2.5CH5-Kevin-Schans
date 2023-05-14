29fd0160400321b5e3980d6890aec7923f6b5ac3
Kevin Schans <kevinvdsd@hotmail.com>
2023-05-14 15:31:14 +0200

    Add create-table function

---
+++ gameOfLife.js
@@ -0,0 +1,30 @@
+const createGameTable = (rows, columns) => {
+  let insertTableHTML = '<table>';
+
+  for (let i = 0; i < rows; i++) {
+      insertTableHTML += '<tr>';
+          for (let j = 0; j < columns; j++) {
+              insertTableHTML += `<td class="cell-${j + "-" + i}">`
+              insertTableHTML += '</td>'
+          }
+          
+      insertTableHTML += '</tr>';
+  }
+  
+  insertTableHTML += '</table>';
+  const container = document.querySelector('.table-container');
+  container.innerHTML = insertTableHTML;
+};
+
+createGameTable(20, 20);
+
+const changeCellStatus = (x, y) => {
+    const getCell = document.querySelector(`.cell-${x + "-" + y}`);
+    
+    if (getCell.style.background !== 'black') {
+        getCell.style.background = 'black';
+
+    } else {
+        getCell.style.background = '';
+    }
+}
